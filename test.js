c16e1d07a43006b95926385b991e80f4662c369c
luoshishi <825364263@qq.com>
2020-11-05 09:25:20 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,20 @@
+/* var arr = ['"晴天","多云","刮风","小雨","下雪","打雷"']
+var len = arr.length */
+
+/* function get_weather(arr){
+    var len = arr.length
+    var num =Math.floor(Math.random()*len)
+    console.log(num)
+}
+
+get_weather(arr) */
+
+var btn1 = true
+var btn2 = true
+function test(){
+    if(btn1 == true){
+        btn2 = true
+    }else{
+        
+    }
+}
